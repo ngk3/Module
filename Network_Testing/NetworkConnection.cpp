812d9ac6153b113e65301b62b3b31989bdd2276d
Kevin Ng <kevin.ng.ngk3@gmail.com>
2016-08-03 13:07:13 -0400

    Update NetworkConnection.cpp
    Added comments

--- Network_Testing/NetworkConnection.cpp
+++ Network_Testing/NetworkConnection.cpp
@@ -3,6 +3,7 @@
 using namespace Module;
 using namespace std;
 
+// Constructor sets everything default and clears data
 NetworkConnection::NetworkConnection( unsigned int protocolId, float timeout )
 {
 	this->protocolId = protocolId;
@@ -12,12 +13,14 @@ NetworkConnection::NetworkConnection( unsigned int protocolId, float timeout )
 	ClearData();
 }
 
+// Stops the connection if the connection is running
 NetworkConnection::~NetworkConnection()
 {
 	if ( running )
 		Stop();
 }
 
+//Attempts to start a connection by using a socket to connect to a port
 bool NetworkConnection::Start( int port )
 {
 	assert( !running );
@@ -28,6 +31,7 @@ bool NetworkConnection::Start( int port )
 	return true;
 }
 		
+//Resets the connection
 void NetworkConnection::Stop()
 {
 	assert( running );
@@ -36,7 +40,8 @@ void NetworkConnection::Stop()
 	socket.Close();
 	running = false;
 }
-		
+
+// Set connection to be in a server-state mode and listens 		
 void NetworkConnection::Listen()
 {
 	printf( "server listening for connection\n" );
@@ -44,7 +49,8 @@ void NetworkConnection::Listen()
 	mode = Server;
 	state = Listening;
 }
-		
+
+// Attempts to connect to NetworkAddress		
 void NetworkConnection::Connect( const NetworkAddress & address )
 {
 	printf( "client connecting to %d.%d.%d.%d:%d\n", 
@@ -55,12 +61,15 @@ void NetworkConnection::Connect( const NetworkAddress & address )
 	this->address = address;
 }
 
+// Update the connection after deltaTime
 void NetworkConnection::Update( float deltaTime )
 {
 	assert( running );
+	// used to tell if connection has timed out
 	timeoutAccumulator += deltaTime;
 	if ( timeoutAccumulator > timeout )
 	{
+		// Do this (state connection has failed and clear data) if timeout is true
 		if ( state == Connecting )
 		{
 			printf( "connect timed out\n" );
@@ -76,12 +85,15 @@ void NetworkConnection::Update( float deltaTime )
 		}
 	}
 }
-		
+
+// Sending a packet with size 'size\ and sending data[]'		
 bool NetworkConnection::SendPacket( const unsigned char data[], int size )
 {
+	// After making sure that connection is running and the address is accessible...
 	assert( running );
 	if ( address.GetAddress() == 0 )
 		return false;
+	// Sends the packet over the socket using Send
 	unsigned char packet[size+4];
 	packet[0] = (unsigned char) ( protocolId >> 24 );
 	packet[1] = (unsigned char) ( ( protocolId >> 16 ) & 0xFF );
@@ -91,8 +103,10 @@ bool NetworkConnection::SendPacket( const unsigned char data[], int size )
 	return socket.Send( address, packet, size + 4 );
 }
 
+// Receiving a packet with size 'size\'
 int NetworkConnection::ReceivePacket( unsigned char data[], int size )
 {
+	// After making sure conneciton is running and that there is indeed data being sent...
 	assert( running );
 	unsigned char packet[size+4];
 	NetworkAddress sender;
@@ -101,11 +115,13 @@ int NetworkConnection::ReceivePacket( unsigned char data[], int size )
 		return 0;
 	if ( bytes_read <= 4 )
 		return 0;
+	// Takes the packet from the socket
 	if ( packet[0] != (unsigned char) ( protocolId >> 24 ) || 
 		 packet[1] != (unsigned char) ( ( protocolId >> 16 ) & 0xFF ) ||
 		 packet[2] != (unsigned char) ( ( protocolId >> 8 ) & 0xFF ) ||
 		 packet[3] != (unsigned char) ( protocolId & 0xFF ) )
 		return 0;
+	// Prints if connection has been eastablished and/or resets the values needed in the NetworkConnection
 	if ( mode == Server && !IsConnected() )
 	{
 		printf( "server accepts connection from client %d.%d.%d.%d:%d\n", 
@@ -127,6 +143,7 @@ int NetworkConnection::ReceivePacket( unsigned char data[], int size )
 	return 0;
 }
 
+// Resets dataa to default values/ new values
 void NetworkConnection::ClearData()
 {
 	state = Disconnected;
